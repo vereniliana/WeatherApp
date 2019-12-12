c0bd6a5dd291c1b5701e4ded0db9ad7ffaad9d33
vereniliana <vereniliana@gmail.com>
2019-12-12 15:45:38 +0700

    get user location

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,12 +1,121 @@
 package com.example.weatherapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.*
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    val MY_PERMISSIONS_REQUEST_LOCATION = 99
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        getLastLocation()
+    }
+
+    private val locationCallback = object : LocationCallback() {
+        @SuppressLint("SetTextI18n")
+        override fun onLocationResult(locationResult: LocationResult) {
+            var lastLocation: Location = locationResult.lastLocation
+            tv_latitude.text = getString(R.string.lat_label) + lastLocation.latitude.toString()
+            tv_longitude.text = getString(R.string.lat_label) + lastLocation.longitude.toString()
+        }
+    }
+
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermissions() {
+        Log.d("lalala","requestPermission")
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+            MY_PERMISSIONS_REQUEST_LOCATION
+        )
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_LOCATION) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                requestNewLocationData()
+            } else {
+                //TODO permission not granted
+                Toast.makeText(this, "permission not granted", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun getLastLocation() {
+        Log.d("lalala","getLastLoc")
+        if (checkPermissions()) {
+            Log.d("lalala","gll permission granted")
+            if (isLocationEnabled()) {
+                Log.d("lalala","gll loc enabled")
+                fusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                    var location: Location? = task.result
+                    if (location == null) {
+                        requestNewLocationData()
+                    } else {
+                        tv_latitude.text = getString(R.string.lat_label) + location.latitude.toString()
+                        tv_longitude.text = getString(R.string.lat_label) + location.longitude.toString()
+                    }
+                }
+            } else {
+                Log.d("lalala","gll loc not enabled")
+                Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            Log.d("lalala","gll permission not granted")
+            requestPermissions()
+        }
+    }
+
+    private fun requestNewLocationData() {
+        Log.d("lalala","requestNewLocData")
+        val locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 0
+        locationRequest.fastestInterval = 0
+        locationRequest.numUpdates = 1
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest, locationCallback,
+            Looper.myLooper()
+        )
     }
 }
